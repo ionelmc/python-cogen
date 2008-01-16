9b0879e79e1ee8d27417e2ad2a7600cdf5c5615a
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-01-16 09:32:15 +0000

    looks like i forgot a the superclass initialization for epollpoller

--- cogen/core/pollers.py
+++ cogen/core/pollers.py
@@ -203,6 +203,7 @@ class KQueuePoller(Poller):
                     
 class EpollPoller(Poller):
     def __init__(t, scheduler, default_size = 1024):
+        super(t.__class__, t).__init__(scheduler)
         t.scheduler = scheduler
         t.epoll_fd = epoll.epoll_create(default_size)
     def remove(t, op, coro):
