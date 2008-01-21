4c66fa188744e66c85eecdc6867a7540b3772cbe
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-01-21 12:36:23 +0000

    doc fix

--- cogen/core/queue.py
+++ cogen/core/queue.py
@@ -216,15 +216,45 @@ class Queue:
         return self._full()
     
     def put(self, item, block=True, **kws):
+        """Put an item into the queue.
+
+        If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until a free slot is available. If 'timeout' is
+        a positive number, it blocks at most 'timeout' seconds and raises
+        the Full exception if no free slot was available within that time.
+        Otherwise ('block' is false), put an item on the queue if a free slot
+        is immediately available, else raise the Full exception ('timeout'
+        is ignored in that case).
+        """
         return QPut(self, item, block, **kws)
     
     def put_nowait(self, item):
+        """Put an item into the queue without blocking.
+
+        Only enqueue the item if a free slot is immediately available.
+        Otherwise raise the Full exception.
+        """
         return self.put(item, False)
     
     def get(self, block=True, **kws):
+        """Remove and return an item from the queue.
+
+        If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until an item is available. If 'timeout' is
+        a positive number, it blocks at most 'timeout' seconds and raises
+        the Empty exception if no item was available within that time.
+        Otherwise ('block' is false), return an item if one is immediately
+        available, else raise the Empty exception ('timeout' is ignored
+        in that case).
+        """
         return QGet(self, block, **kws)
     
     def get_nowait(self):
+        """Remove and return an item from the queue without blocking.
+
+        Only get an item if one is immediately available. Otherwise
+        raise the Empty exception.
+        """
         return self.get(False)
             
         
