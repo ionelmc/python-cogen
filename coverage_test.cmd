2d5cfe4735e618f97317461899f1c4802ddcc205
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-01-22 00:17:11 +0000

    0.1.5 release

--- coverage_test.cmd
+++ coverage_test.cmd
@@ -1,3 +1,4 @@
 python -m coverage -e
-python -m coverage -x cogen\test\test_queue.py
-python -m coverage -r -m -o d:\python
+python -m coverage -x setup.py test
+REM ~ python -m coverage -a -d coverage -o d:\python,lib
+python -m coverage -r -m -o d:\python,lib
