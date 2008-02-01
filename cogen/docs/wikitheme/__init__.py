4337cbe528aeedb954ba1b97fb5541c47a9f6560
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-02-01 21:19:07 +0000

    new codewiki theme

---
+++ cogen/docs/wikitheme/__init__.py
@@ -0,0 +1,12 @@
+from apydia.descriptors import AttributeOrMethodDesc
+
+def href(desc, anchor=None):
+    if isinstance(desc, AttributeOrMethodDesc):
+        return "#" + desc.name
+    else:
+        return "".join(path.name.capitalize() for path in desc.path)
+
+
+def filename(desc):
+    return "".join(path.name.capitalize() for path in desc.path) + ".wiki"
+
