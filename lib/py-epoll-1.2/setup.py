3cb196b55a40b8939cd9d819db3b84d023f89cb1
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-04-09 07:02:14 +0000

    added zip_safe=False to suppress some warnings (the .so was extracted to a temp location in the users dir)

--- lib/py-epoll-1.2/setup.py
+++ lib/py-epoll-1.2/setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.core import Extension
 
 module1 = Extension('epoll',
                     sources = ['epollmodule.c'])
 
 setup (name = 'py-epoll',
-       version = '1.2',
+       version = '1.2.1',
        description = 'A Python interface to epoll(4)',
        author='Ben Woolley', author_email='user ben at host tautology.org',
-       ext_modules = [module1])
+       ext_modules = [module1],
+       zip_safe=False,
+       )
--- lib/py-kqueue-2.0/setup.py
+++ lib/py-kqueue-2.0/setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.core import Extension
 
 module1 = Extension('kqueuemodule',
                     sources = ['kqueuemodule.c'])
 
 setup (name = 'py-kqueue',
-       version = '2.0',
+       version = '2.0.1',
        description = 'This is a kqueue package',
        author='Jaromir Dolecek',author_email='jdolecek@netbsd.org',
-       ext_modules = [module1])
+       ext_modules = [module1],
+       zip_safe=False,
+       )
