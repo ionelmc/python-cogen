cd000eb2292c8582f10eec7f6878e858481d0abd
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-05-02 23:05:12 +0000

    added some internals info in the docstring

--- cogen/__init__.py
+++ cogen/__init__.py
@@ -4,12 +4,63 @@ This is a library for network oriented, coroutine based programming.
 The interfaces and events/operations aim to mimic some of the regular thread 
 and socket features. 
 
-Coroutines work as simple generators, the operations and events work as objects 
-passed in and out of the generator, these objects are managed by the scheduler
-and reactor. 
+cogen uses the `enhanced generators <http://www.python.org/dev/peps/pep-0342/>`_
+in python 2.5. These generators are bidirectional: they allow to pass values in 
+and out of the generator. The whole framework is based on this.
+
+The generator yields a `Operation` instance and will receive the result from 
+that yield when the operation is ready.
+
+::
+    
+    Roughly the cogen internals works like this:
+
+    +------------------------+
+    | @coroutine             |
+    | def foo():             |
+    |     ...                |             op.process(sched, coro)
+    |  +->result = yield op--|----------------+------------+
+    |  |  ...                |                |            |  
+    +--|---------------------+    +---------------+  +---------------------+      
+       |                          | the operation |  | the operation can't |
+      result = op.finalize()      | is ready      |  | complete right now  |
+       |                          +------|--------+  +----------|----------+
+      scheduler runs foo                 |                      |
+       |                                 |                      |
+      foo gets in the active             |                      |
+      coroutines queue                   |                      |
+       |                                 |                      |
+       +----------------------<----------+                      |
+       |                                                    depening on the op      
+      op.run()                                               +---------+
+       |      socket is ready               add it in        |         |
+       +-------------<------------  ......  the reactor   <--+         |
+       |                         later                                 | 
+       +------<-------------------  ......  add it in some other     <-+
+        some event decides                  queue for later run
+        this op is ready
+        
+        
+    The scheduler basicaly does 3 things:
+     - runs active (coroutine,operations) pairs (calls process on the op)
+     - runs the reactor
+     - checks for timeouts
+     
+    The reactor basicaly does 2 things:
+     - calls the system to check what descriptors are ready
+     - runs the operations that have ready descriptors
+     
+    The operation does most of the work (via the process, finalize, cleanup, 
+    run methods):
+     - adds itself in the reactor (if it's a socket operation)
+     - adds itself in some structure to be activated later by some other event
+     - adds itself and the coro in the scheduler's active coroutines queue
+
+    The coroutine decorator wrappes foo in a Coroutine class that does some
+    niceties like exception handling, getting the result from finalize() etc.
 '''
 
-__license__ = '''
+__license__ = u'''
 Copyright (c) 2007, Mărieş Ionel Cristian
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -32,7 +83,7 @@ THE SOFTWARE.
 '''
 
 __author__ = u"Mărieş Ionel Cristian"
-__email__ = "ionel dot mc at gmail dot com"
+__email__ = "ionel.mc@gmail.com"
 __revision__ = "$Revision$"
 __version__ = '0.1.8'
 __svnid__ = "$Id$"
