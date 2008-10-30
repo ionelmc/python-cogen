12e9f0e092fc61173c73f0a45c4010aee055feae
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-10-30 16:31:39 +0000

    changed the wrappers a bit, added proactor code from the native iocp

--- cogen/core/proactors/ctypes_iocp_impl/api_wrappers.py
+++ cogen/core/proactors/ctypes_iocp_impl/api_wrappers.py
@@ -4,19 +4,12 @@ from ctypes import c_int, c_uint, c_long, c_ulong, c_void_p, byref, c_char_p
 from ctypes import c_ushort, c_ubyte, c_char, WINFUNCTYPE, c_short, c_ubyte
 from ctypes import Structure, Union, py_object, POINTER, pointer, sizeof
 from ctypes.wintypes import HANDLE, ULONG, DWORD, BOOL, LPCSTR, LPCWSTR, WinError
+
+from api_consts import *
+
 import os
 
 NULL = c_ulong()
-
-IOC_WS2 = 0x08000000
-IOC_OUT = 0x40000000
-IOC_IN = 0x80000000
-IOC_INOUT = IOC_IN | IOC_OUT
-SIO_GET_EXTENSION_FUNCTION_POINTER =  IOC_INOUT | IOC_WS2 | 6
-SOL_SOCKET = 0xffff
-SO_PROTOCOL_INFOA = 0x2004
-SO_PROTOCOL_INFOW = 0x2005
-
 SOCKET = SIZE_T = c_uint
 
 class _US(Structure):
@@ -43,7 +36,7 @@ class OVERLAPPED(Structure):
         ("hEvent",          HANDLE),
 
         # Custom fields.
-        ("channel",         py_object),
+        ("object",         py_object),
     ]
 
     _anonymous_ = ("u",)
@@ -70,9 +63,11 @@ class GUID(Structure):
 
 WSAID_CONNECTEX = GUID(0x25a207b9,0xddf3,0x4660,0x8e,0xe9,0x76,0xe5,0x8c,0x74,0x06,0x3e)
 WSAID_ACCEPTEX = GUID(0xb5367df1,0xcbac,0x11cf,0x95,0xca,0x00,0x80,0x5f,0x48,0xa1,0x92)
+WSAID_TRANSMITFILE = GUID(0xb5367df0,0xcbac,0x11cf,0x95,0xca,0x00,0x80,0x5f,0x48,0xa1,0x92)
 
 MAX_PROTOCOL_CHAIN = 7
 WSAPROTOCOL_LEN = 255
+
 class WSAPROTOCOLCHAIN(Structure):
     _fields_ = [
         ('ChainLen', c_int),
@@ -101,6 +96,14 @@ class WSAPROTOCOL_INFO(Structure):
         ('dwMessageSize', DWORD),
         ('dwProviderReserved', DWORD),
         ('szProtocol', c_char * (WSAPROTOCOL_LEN+1)),
+    ]
+    
+class TRANSMIT_FILE_BUFFERS(Structure):
+    _fields_ = [
+        ('Head', c_void_p),
+        ('HeadLength', DWORD),
+        ('Tail', c_void_p),
+        ('TailLength', DWORD)
     ]
  
 class sockaddr(Structure):
@@ -140,60 +143,89 @@ addrinfo._fields_ = [
     ('ai_next', POINTER(addrinfo))
 ]
 
-def _error_check(result, func, args):
+def _error_throw(result, func, args):
     if result:
         raise WinError()
     return result
 
-def _bool_error_check(result, func, args):
+def _bool_error_throw(result, func, args):
     if not result:
         raise WinError()
+    return result
+
+def _error_check(result, func, args):
+    if result:
+        return GetLastError()
+    return result
+
+def _bool_error_check(result, func, args):
+    if not result:
+        return GetLastError()
     return result
     
 CreateIoCompletionPort = windll.kernel32.CreateIoCompletionPort
 CreateIoCompletionPort.argtypes = (HANDLE, HANDLE, POINTER(c_ulong), DWORD)
 CreateIoCompletionPort.restype = HANDLE
+CreateIoCompletionPort.errcheck = _bool_error_throw
 
 GetQueuedCompletionStatus = windll.kernel32.GetQueuedCompletionStatus
 GetQueuedCompletionStatus.argtypes = (HANDLE, POINTER(DWORD), POINTER(c_ulong), POINTER(POINTER(OVERLAPPED)), DWORD)
 GetQueuedCompletionStatus.restype = BOOL
+GetQueuedCompletionStatus.errcheck = _bool_error_check
+
+PostQueuedCompletionStatus = windll.kernel32.PostQueuedCompletionStatus
+# BOOL = HANDLE CompletionPort, DWORD dwNumberOfBytesTransferred, ULONG_PTR dwCompletionKey, LPOVERLAPPED lpOverlapped
+PostQueuedCompletionStatus.argtypes = (HANDLE, DWORD, POINTER(c_long), POINTER(OVERLAPPED))
+PostQueuedCompletionStatus.restype = BOOL
+PostQueuedCompletionStatus.errcheck = _bool_error_check
+
+CancelIo = windll.kernel32.CancelIo
+# BOOL = HANDLE hFile
+CancelIo.argtypes = (HANDLE,)
+CancelIo.restype = BOOL
+CancelIo.errcheck = _bool_error_check
 
 getaddrinfo = windll.ws2_32.getaddrinfo
-# const char *nodename, const char *servname, const struct addrinfo *hints, struct addrinfo **res
+# int = const char *nodename, const char *servname, const struct addrinfo *hints, struct addrinfo **res
 getaddrinfo.argtypes = (c_char_p, c_char_p, POINTER(addrinfo), POINTER(POINTER(addrinfo)))
 getaddrinfo.restype = c_int
-getaddrinfo.errcheck = _error_check
+getaddrinfo.errcheck = _error_throw
 
 getsockopt = windll.ws2_32.getsockopt
-# SOCKET s, int level, int optname, char *optval, int *optlen
+# int = SOCKET s, int level, int optname, char *optval, int *optlen
 getsockopt.argtypes = (SOCKET, c_int, c_int, c_char_p, POINTER(c_int))
 getsockopt.restype = c_int
-getsockopt.errcheck = _error_check
+getsockopt.errcheck = _error_throw
 
 WSARecv = windll.ws2_32.WSARecv
-# SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesRecvd, LPDWORD lpFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
+# int = SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesRecvd, LPDWORD lpFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
 WSARecv.argtypes = (SOCKET, POINTER(WSABUF), DWORD, POINTER(DWORD), POINTER(DWORD), POINTER(OVERLAPPED), c_void_p)
 WSARecv.restype = c_int
 
 WSASend = windll.ws2_32.WSASend
-# SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
+# int = SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
 WSASend.argtypes = (SOCKET, POINTER(WSABUF), DWORD, POINTER(DWORD), DWORD, POINTER(OVERLAPPED), c_void_p)
 WSASend.restype = c_int
-
+WSASend.errcheck = _error_check
 
 AcceptEx = windll.Mswsock.AcceptEx
-# SOCKET sListenSocket, SOCKET sAcceptSocket, PVOID lpOutputBuffer, DWORD dwReceiveDataLength, DWORD dwLocalAddressLength, DWORD dwRemoteAddressLength, LPDWORD lpdwBytesReceived, LPOVERLAPPED lpOverlapped
+# BOOL = SOCKET sListenSocket, SOCKET sAcceptSocket, PVOID lpOutputBuffer, DWORD dwReceiveDataLength, DWORD dwLocalAddressLength, DWORD dwRemoteAddressLength, LPDWORD lpdwBytesReceived, LPOVERLAPPED lpOverlapped
 AcceptEx.argtypes = (SOCKET, SOCKET, c_void_p, DWORD, DWORD, DWORD, POINTER(DWORD), POINTER(OVERLAPPED))
 AcceptEx.restype = BOOL
 AcceptEx.errcheck = _bool_error_check
 
-# SOCKET s, DWORD dwIoControlCode, LPVOID lpvInBuffer, DWORD cbInBuffer, LPVOID lpvOutBuffer, DWORD cbOutBuffer, LPDWORD lpcbBytesReturned, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
+GetAcceptExSockaddrs = windll.Mswsock.GetAcceptExSockaddrs
+# void = PVOID lpOutputBuffer, DWORD dwReceiveDataLength, DWORD dwLocalAddressLength, DWORD dwRemoteAddressLength, LPSOCKADDR *LocalSockaddr, LPINT LocalSockaddrLength, LPSOCKADDR *RemoteSockaddr, LPINT RemoteSockaddrLength
+GetAcceptExSockaddrs.argtypes = (c_void_p, DWORD, DWORD, DWORD, POINTER(sockaddr), POINTER(c_int), POINTER(sockaddr), POINTER(c_int))
+GetAcceptExSockaddrs.restype = None
+
+# int = SOCKET s, DWORD dwIoControlCode, LPVOID lpvInBuffer, DWORD cbInBuffer, LPVOID lpvOutBuffer, DWORD cbOutBuffer, LPDWORD lpcbBytesReturned, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
 WSAIoctl = windll.ws2_32.WSAIoctl
 WSAIoctl.argtypes = (SOCKET, DWORD, c_void_p, DWORD, c_void_p, DWORD, POINTER(DWORD), POINTER(OVERLAPPED), c_void_p)
 WSAIoctl.restype = c_int
-WSAIoctl.errcheck = _error_check
+WSAIoctl.errcheck = _error_throw
 
-# SOCKET s, const struct sockaddr *name, int namelen, PVOID lpSendBuffer,
+# BOOL = SOCKET s, const struct sockaddr *name, int namelen, PVOID lpSendBuffer,
 ConnectExType = WINFUNCTYPE(BOOL, c_int, POINTER(sockaddr), c_int, c_void_p, DWORD, POINTER(DWORD), POINTER(OVERLAPPED))
 
 def _GetConnectExPtr(given_socket=None):
@@ -208,23 +240,48 @@ def _GetConnectExPtr(given_socket=None):
     return ConnectEx
 
 ConnectEx = _GetConnectExPtr()
-#~ ConnectEx.errcheck = _bool_error_check
+ConnectEx.errcheck = _bool_error_check
+
+#~ BOOL = SOCKET hSocket, HANDLE hFile, DWORD nNumberOfBytesToWrite, DWORD nNumberOfBytesPerSend, LPOVERLAPPED lpOverlapped,LPTRANSMIT_FILE_BUFFERS lpTransmitBuffers, DWORD dwFlags
+TransmitFileType = WINFUNCTYPE(BOOL, SOCKET, HANDLE, DWORD, DWORD, POINTER(OVERLAPPED), TRANSMIT_FILE_BUFFERS, DWORD)
+
+def _GetTransmitFilePtr(given_socket=None):
+    from socket import socket
+    bogus_sock = given_socket or socket()
+    bogus_bytes = DWORD()
+    TransmitFile = TransmitFileType(0)
+    ret = WSAIoctl(
+        bogus_sock.fileno(), SIO_GET_EXTENSION_FUNCTION_POINTER, byref(WSAID_TRANSMITFILE), sizeof(WSAID_TRANSMITFILE),
+        byref(TransmitFile), sizeof(TransmitFile), byref(bogus_bytes), None, None
+    )
+    return TransmitFile
+
+TransmitFile = _GetTransmitFilePtr()
+TransmitFile.errcheck = _bool_error_check
 
 CloseHandle = windll.kernel32.CloseHandle
 CloseHandle.argtypes = (HANDLE,)
 CloseHandle.restype = BOOL
 
+_get_osfhandle = windll.msvcr71._get_osfhandle
+_get_osfhandle.argtypes = (c_int,)
+_get_osfhandle.restype = c_int
+
 # Python API
 
 pythonapi.PyBuffer_New.argtypes = (c_ulong,)
 pythonapi.PyBuffer_New.restype = py_object
+AllocateBuffer = pythonapi.PyBuffer_New
 
 pythonapi.PyErr_SetFromErrno.argtypes = (py_object,)
 pythonapi.PyErr_SetFromErrno.restype = py_object
 
 # ----------------------------------------------------------------------------
-
-if __name__ == "__main__":
+#~ if __name__ == "__main__":
+    #~ x = pythonapi.PyBuffer_New(100)
+    #~ print `x`
+    
+if __name__ == "x__main__":
     import socket
     s = socket.socket()
     prot_info = WSAPROTOCOL_INFO()
