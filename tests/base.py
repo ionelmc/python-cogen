11c6e1b5f8a7476919ab0b375fc87abf93ab1690
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-11-24 08:24:58 +0000

    added ctypes iocp to tests

--- tests/base.py
+++ tests/base.py
@@ -15,13 +15,14 @@ class PrioCORO:
     prio = priority.CORO
     
 priorities = (PrioCORO, PrioOP, PrioFIRST, PrioLAST)
-from cogen.core.proactors import has_iocp, \
+from cogen.core.proactors import has_iocp, has_ctypes_iocp, \
                                 has_kqueue, has_stdlib_kqueue, \
                                 has_epoll, has_stdlib_epoll, \
                                 has_poll, has_select
 proactors_available = [
     j for j in [
         i() for i in (
+            has_ctypes_iocp,
             has_iocp,
             has_stdlib_kqueue,             
             has_kqueue, 
