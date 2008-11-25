647e95068e3158d7ae207b6f991207a917f66b91
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-11-25 10:47:59 +0000

    changed start number

--- loop-run.cmd
+++ loop-run.cmd
@@ -2,9 +2,9 @@
 set /a count=0
 set /a fails=0
 :do
-set /a count=%count%+1
 echo runs: %count% fails: %fails% command: %*
 %*
+set /a count=%count%+1
 if %errorlevel%==0 goto do
 set /a fails=%fails%+1
 goto do
