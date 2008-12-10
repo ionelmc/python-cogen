5e457995971a71a58c2fd998e3bd8d0c32755ddd
Ionel Cristian Mărieș <contact@ionelmc.ro>
2008-12-10 13:21:37 +0000

    changed the default proactor order for windows

--- cogen/core/proactors/__init__.py
+++ cogen/core/proactors/__init__.py
@@ -89,6 +89,6 @@ def get_first(*imps):
 
 def has_any():
     "Returns the best available proactor implementation for the current platform."
-    return get_first(has_iocp, has_ctypes_iocp, has_kqueue, has_stdlib_epoll, has_epoll, has_poll, has_select)
+    return get_first(has_ctypes_iocp, has_iocp, has_kqueue, has_stdlib_epoll, has_epoll, has_poll, has_select)
 
 DefaultProactor = has_any()
