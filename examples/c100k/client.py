948611e39878861391191ff4ce377b45ae64ac28
Ionel Cristian Mărieș <contact@ionelmc.ro>
2009-01-16 12:58:02 +0000

    made some optimisations to the examples

--- examples/c100k/client.py
+++ examples/c100k/client.py
@@ -1,33 +1,37 @@
 import sys, os, traceback, socket
 from cogen.common import *
 
-m = Scheduler(proactor_multiplex_first=False, ops_greedy=True, default_priority=priority.FIRST, proactor_default_size=102400)
+m = Scheduler(
+    proactor_multiplex_first=False, 
+    ops_greedy=True, 
+    default_priority=priority.FIRST, 
+    proactor_default_size=102400, 
+    proactor_greedy=False,
+    proactor_resolution=5
+)
 port = len(sys.argv)>1 and int(sys.argv[1]) or 1200
+num = 0
 
 @coroutine
-def client():
-    socket_list = []
-    for i in xrange(50000):
-        sock = sockets.Socket()
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("192.168.111.203", 10000+i))
-        socket_list.append(sock)
-        try:
-            yield sock.connect(("192.168.111.201", port))
-        except:
-            traceback.print_exc()
-            return
-    for i in xrange(50000):
-        sock = sockets.Socket()
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("192.168.111.204", 10000+i))
-        socket_list.append(sock)
-        try:
-            yield sock.connect(("192.168.111.202", port))
-        except:
-            traceback.print_exc()
-            return
-
-m.add(client)
+def client(local, remote):
+    global num
+    sock = sockets.Socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(local)
+    yield sock.connect(remote)
+    num += 1
+    if num == 100000:
+        # actually we don't need to revive 100k coros
+        sys.exit()
+        
+        yield events.Signal('done')
+    else:
+        yield events.WaitForSignal('done')
+    
+        
+for i in xrange(50000):
+    m.add(client, args=(("192.168.111.203", 10000+i), ("192.168.111.201", port)))
+for i in xrange(50000):
+    m.add(client, args=(("192.168.111.204", 10000+i), ("192.168.111.202", port)))
 
 m.run()
